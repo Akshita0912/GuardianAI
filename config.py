5a2dd61b36d5dbf05bfa6100e64d1bd21e249700
Akshita Bhamidipati <akshitabhamidipati@gmail.com>
2025-09-21 18:32:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+
 EMERGENCY_CONTACTS = [
     {"name": "Parent", "phone": "+911234567890"},
     {"name": "Friend", "phone": "+919876543210"}
 ]
 
+# Hospitals & Police
 HOSPITAL_API = "https://api.hospital-alerts.com/notify"
 POLICE_API = "https://api.police-alerts.com/notify"
 
-ALERT_THRESHOLD = 0.7   # Emotion probability threshold
-LOCATION = "Bengaluru, India"  # Change to GPS-based later
+# Alert settings
+ALERT_THRESHOLD = 0.7
+LOCATION = "Hyderabad, India" 
